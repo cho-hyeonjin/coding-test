cc85140acbf24248d83998c65f91a4274b897bfd
HyeonJinCho <78816754+cho-hyeonjin@users.noreply.github.com>
2023-10-27 00:14:26 +0900

    Update 머쓱이보다 키 큰 사람.js

--- 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,5 +1,5 @@
 function solution(array, height) {
-    // array를 돌 때마다 heigth와의 대소비교를 한다.
+    // array를 돌 때마다 heigt와의 대소비교를 한다.
     // height보다 큰 경우만 배열로 리턴하면 되기 때문에, filter나 forEach를 써도 괜찮을 것 같다.
     var answer = array.filter((el) => height < el )
     return answer.length;
