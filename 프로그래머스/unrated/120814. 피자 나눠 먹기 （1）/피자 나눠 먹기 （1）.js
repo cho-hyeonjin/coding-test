656f047a96cf53dfb92fc2a1963f6e84167a80e2
HyeonJinCho <78816754+cho-hyeonjin@users.noreply.github.com>
2023-10-27 11:33:50 +0900

    [level unrated] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,14 +1,18 @@
 function solution(n) {
-    // n을 7로 나눴을때의 나머지가 7미만이면 몫+1판
-    // n이 7 이하면 1판
     
-    var answer = 0;
+    // n이 7 이하면 1판
+    // n을 7로 나눴을때의 나머지가 7미만이면 몫+1판
+    // 그 외에는 몫 판
+    
+//     var answer = 0;
 
-    if (n <= 7){
-        return  answer = 1;
-    }
-    else if(0 < n % 7 && n % 7 < 7) {
-       return answer = Math.floor(n / 7) + 1;
-    } return answer = Math.floor(n/7);
-
+//     if (n <= 7){
+//         return  answer = 1;
+//     }
+//     else if(0 < n % 7 && n % 7 < 7) {
+//        return answer = Math.floor(n / 7) + 1;
+//     } return answer = Math.floor(n/7);
+    
+     // 간단하게 몫을 반올림처리 하면 코드가 훨씬 간결해짐!
+    return Math.ceil(n / 7)
 }
