f9fae0ed7828d2c7f022a5e5dad06cff194b3aa3
HyeonJinCho <78816754+cho-hyeonjin@users.noreply.github.com>
2023-11-07 14:57:34 +0900

    [level unrated] Title: A로 B 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/unrated/120886. A로 B 만들기/A로 B 만들기.js
@@ -4,14 +4,5 @@
 // before.split('').sort() 하면 알파벳순으로 정렬
 // after.split('').sort() 한 것과 같으면 1 다르면 0 리턴
 function solution(before, after) {
-    let sortedBefore = before.split('').sort();
-    let sortedAfter= after.split('').sort()
-    
-    let answer = 0;
-    sortedBefore.join('') === sortedAfter.join('') ? answer = 1 : answer = 0;
-    
-    console.log(sortedBefore, sortedAfter, answer)
-    // 왜 틀리는지 생각해보니, sortedBefore과 sortedAfter는 참조형 데이터여서 false가 나온다고 생각되었다.
-    // join메서드로 String으로 변경하여 진행해보기로 했다.
-    return answer
+    return before.split('').sort().join('') === after.split('').sort().join('') ? 1 : 0;
 }
