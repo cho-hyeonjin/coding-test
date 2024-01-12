3ac2b64f30eb5ffd921eb889da874cd68b7a4803
HyeonJinCho <78816754+cho-hyeonjin@users.noreply.github.com>
2024-01-12 10:44:20 +0900

    [level Lv.0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/Lv.0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -8,4 +8,8 @@ const solution = (n) => {
     }
     return result
     // 하지만 문제에 "오름차순으로 정렬"이라는 조건을 명시해둔 것을 보면, sort 메서드를 사용한 코드가 더 좋은 것 같다.
+    
+    // let result = [];
+    // for(let i = 1; i <= n; i+=2) result.push(i) // 반복문 조건 설정을 이용해서 푸는 방식도 생각해보자. 조건 설정을 습관적으로, 기계적으로 하지 말기~ 
+    // return result;
 }
