16e2c328119f8b805377f7f65b2b2b9f4518b43c
HyeonJinCho <78816754+cho-hyeonjin@users.noreply.github.com>
2024-01-13 16:58:30 +0900

    [level Lv.0] Title: 삼각형의 완성조건 (1), Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/Lv.0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,13 +1,15 @@
 const solution = (sides) => {
-    // // Math.max()로 최대값을 찾아서 해결해보기
-    // let longest = Math.max(...sides)
-    // // longest를 제외한 두 수를 찾고
-    // // 그 다음에 reduce
-    // // side가 longest와 같지 않다면이 아니라, longest보다 크지 않다면 === 작거나 같다면
-    // let rests = sides.filter((side) => side !== longest)
-    // let sumRests = rests.reduce((acc, curr) => acc + curr, 0)
-    // // longest(가장 긴 변)은 sumRests(다른 두 변의 길이의 합)보다 작아야 1을 리턴할 수 있다.
-    // return longest < sumRests ? 1 : 2
-    sides.sort((a, b) => b - a);
-    return sides[0] < sides[1] + sides[2] ? 1 : 2    
+    let longest = Math.max(...sides);
+    // (기존)
+    // let rests = sides.filter((side) => side !== longest);
+    // let sumRests = rests.reduce((acc, curr) => acc + curr, 0);
+    // --> 기존 방식처럼 원본 배열에서 최대값을 제거한 배열을 구해서 해당 배열의 모든 요소를 누산하는 방식이 아니라.
+    // --> 원본 배열의 모든 값을 누산한 뒤 최대값을 빼주는 방식으로 수정
+    let sumRests = sides.reduce((acc, curr) => acc + curr, 0) - longest; 
+    
+    return longest < sumRests ? 1 : 2
+    
+    // 또 다른 풀이
+    // sides.sort((a, b) => b - a);
+    // return sides[0] < sides[1] + sides[2] ? 1 : 2    
 }
