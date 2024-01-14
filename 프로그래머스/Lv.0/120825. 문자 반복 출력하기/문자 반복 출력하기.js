e06bd25c89cad838b2624166845feec225d24652
HyeonJinCho <78816754+cho-hyeonjin@users.noreply.github.com>
2024-01-14 21:25:20 +0900

    [level Lv.0] Title: 문자 반복 출력하기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/Lv.0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,3 +1,3 @@
-const solution = (my_string, n) => Array.from(my_string).map((el)=>el.repeat(n)).join('')
+const solution = (my_string, n) => Array.from(my_string).map((el) => el.repeat(n)).join('')
 
 
