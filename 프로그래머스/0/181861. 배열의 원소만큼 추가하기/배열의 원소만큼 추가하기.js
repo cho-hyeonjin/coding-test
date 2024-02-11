24544f7e95039d05fff5124889e1775a8fabab87
HyeonJinCho <78816754+cho-hyeonjin@users.noreply.github.com>
2024-02-11 12:29:57 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.js
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.js
@@ -2,6 +2,7 @@
 
 const solution = (arr) => {
     let X = [];
+    // 첫번째 풀이 - 이중 for문
     // // arr는 숫자 배열
     // // arr[i]를 순회하면서 i에 해당하는 회수만큼 X에 push
     // // 이중 반복문
@@ -14,11 +15,15 @@ const solution = (arr) => {
     // }
     // return X;
     
-    // 두번째 풀이 - map메서드 이용하기
-    arr.map((el) => {
-        for(let i = 0; i < el; i++){
-            X.push(el)
-        }
-    })
-    return X;
+    // 두번째 풀이 - map 메서드
+    // arr.map((el) => {
+    //     for(let i = 0; i < el; i++){
+    //         X.push(el)
+    //     }
+    // })
+    // return X;
+    
+    // 세번째 풀이 - reduce 메서드
+    // 초기값은 빈배열, 누산기에 이전 순회에서 추가한 요소까지 추가되어 있는 배열, 현재값에 지금 순회에서 추가할 요소
+    return arr.reduce((accumulator, currentValue) => [...accumulator, ...new Array(currentValue).fill(currentValue)], [])
 }
